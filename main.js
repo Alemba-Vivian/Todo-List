d4304047db428b53e8a0794571abfd32d8d36427
Alemba-Vivian <alembavivian@gmail.com>
2023-09-04 13:11:04 +0300

    installed webpack together with the necessary files needed for it to bundle the final output

---
+++ main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+alert("hello vivian alemba");
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLDZCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kby1saXN0Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImFsZXJ0KFwiaGVsbG8gdml2aWFuIGFsZW1iYVwiKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
